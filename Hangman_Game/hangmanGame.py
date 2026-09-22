ee9658511c0035d65940ff39d1d58fe9ae56b230
itsprathammukati <iamprathammukati@gmail.com>
2026-09-22 14:11:11 +0530

    Add three hangman files.

--- Hangman_Game/hangmanGame.py
+++ Hangman_Game/hangmanGame.py
@@ -1,14 +1,15 @@
 import random
-from lists import wordList
+from hangmanSteps import hangmanSteps
+from wordList import wordList
 
 # if user entered guess is one character and alphabet but already guessed
 word = random.choice(wordList)
-wordInListFormat = list(correctWord)
+wordInListFormat = list(word)
 
 guessList = ['_'] * len(wordInListFormat)
 
 maxAttempts = 6
-
+steps = 0
 while maxAttempts > 0:
 
     guess = input("Enter your guess: ").lower()
@@ -27,16 +28,17 @@ while maxAttempts > 0:
                         if guess == word[i]:
                             guessList[i] = word[i]
                     print(guessList)
-                    if guessList != word:
-                        print(f"You have {maxAttempts}/12 maxAttempts remaining.")
+                    if guessList != wordInListFormat:
+                        print(f"You have {maxAttempts}/6 maxAttempts remaining.")
                 else:
-                    print("Uhuhh your guess was incorrect.")
+                    print(hangmanSteps[steps])
+                    steps += 1
                     maxAttempts -= 1
-                    print(f"You have {maxAttempts}/12 maxAttempts remaining.")
+                    print(f"You have {maxAttempts}/6 maxAttempts remaining.")
 
-    if guessList == word:
-        print(f"WOHOOO!!! YOU WIN. {correctWord.upper()} is the correct word.")
+    if guessList == wordInListFormat:
+        print(f"WOHOOO!!! YOU WIN. {word.upper()} is the correct word.")
         break
     
 if maxAttempts == 0:
-    print(f"You lose. {correctWord.upper()} was the correct word.")
+    print(f"You lose. {word.upper()} was the correct word.")
--- Hangman_Game/hangmanSteps.py
+++ Hangman_Game/hangmanSteps.py
@@ -1,4 +1,6 @@
-hangmanSteps = [r'''
+hangmanSteps = [
+  
+r'''
   +---+
   |   |
   O   |
